b5b3dde1dba95f8abaaf36736b39367104736f05
romain.fontugne <romain.fontugne@gmail.com>
2018-06-19 14:25:44 +0900

    better management of track bins

--- src/raclette.py
+++ src/raclette.py
@@ -2,12 +2,9 @@ import sys
 import logging
 import datetime
 from collections import defaultdict
-from itertools import combinations, product
-import numpy as np
 import cPickle as pickle
 import ConfigParser
 from multiprocessing import Pool
-import gc
 
 from dumpReader import DumpReader
 from atlasrestreader import AtlasRestReader
@@ -15,9 +12,12 @@ from atlasrestreader import AtlasRestReader
 from firsthoptimetrack import FirstHopTimeTrack
 from astimetrack import ASTimeTrack
 # from iptimetrack import IPTimeTrack
+from tracksmanager import TracksManager
+
 
 def defaultdictlist(): 
     return defaultdict(list)
+
 
 def valid_date(s):
     try:
@@ -31,12 +31,12 @@ def valid_date(s):
 config = ConfigParser.ConfigParser()
 config.read("conf/raclette.conf")
 
-window_size = int(config.get("main", "window_size"))
 
 atlas_start =  valid_date(config.get("traceroute", "start"))
 atlas_stop =  valid_date(config.get("traceroute", "stop"))
 atlas_msm_ids =  [int(x) for x in config.get("traceroute", "msm_ids").split(",") if x]
 atlas_probe_ids =  [int(x) for x in config.get("traceroute", "probe_ids").split(",") if x]
+atlas_chunk_size = int(config.get("traceroute","chunk_size"))
 
 dump_name =  config.get("traceroute", "dump_file")
 dump_filter =  config.get("traceroute", "filter")
@@ -46,7 +46,8 @@ add_probe = config.get("timetrack", "add_probe")
 ip2asn_dir = config.get("lib", "ip2asn_directory")
 ip2asn_db = config.get("lib", "ip2asn_db")
 
-gc_expiration = int(config.get("garbagecollector", "expiration"))
+tm_expiration = int(config.get("tracksmanager", "expiration"))
+tm_window_size = int(config.get("tracksmanager", "window_size"))
 
 # Initialisation
 pool = Pool(processes=6)
@@ -55,87 +56,39 @@ import ip2asn
 i2a = ip2asn.ip2asn(ip2asn_db)
 fhtt = FirstHopTimeTrack(i2a)
 astt = ASTimeTrack(i2a)
-window_start = 0
-# history_size = 10
-track_bins = defaultdict(list)
-bins_last_insert= defaultdict(int)
+tm = TracksManager(tm_window_size, tm_expiration)
 nb_err_traceroutes = 0
-nb_expired_traceroutes = 0
 nb_total_traceroutes = 0
 
 results = defaultdict(defaultdictlist)
-results_bins = defaultdict(defaultdictlist)
 dates = []
 
-with AtlasRestReader(atlas_start, atlas_stop, atlas_msm_ids, atlas_probe_ids, pool=pool) as tr_reader:
+with AtlasRestReader(atlas_start, atlas_stop, atlas_msm_ids, atlas_probe_ids, chunk_size=atlas_chunk_size, pool=pool) as tr_reader:
 # with DumpReader(dump_name, dump_filter) as tr_reader:
 
     # # Main Loop:
     for trace in tr_reader:
         nb_total_traceroutes += 1
-        bin_id = int(trace["timestamp"]/window_size)
-        # print("\r {} traceroutes processed".format(nb_total_traceroutes)
         
-	if trace is None or "error" in trace["result"][0] or "err" in trace["result"][0]["result"]:
+        # Check if the traceroute is valid
+        if trace is None or "error" in trace["result"][0] or "err" in trace["result"][0]["result"]:
             nb_err_traceroutes += 1
             continue
 
-        if bins_last_insert[bin_id] is None:
-            nb_expired_traceroutes += 1
-            continue
-
+        # Make a track from the traceroute
         # track = fhtt.traceroute2timetrack(trace)
         track = astt.traceroute2timetrack(trace)
-        if track is None:
-            nb_err_traceroutes += 1
-            continue
 
         if add_probe:
             track["rtts"] = [("pid_{}".format(track["prb_id"]), [0])] + track["rtts"]
+        
+        tm.add_track(track) 
+        tm.collect_results(results, dates)
 
-        # index track based on its timestamp
-        track_bins[bin_id].append(track)
-        bins_last_insert[bin_id] = nb_total_traceroutes
+        if nb_total_traceroutes % tm_expiration == 0:
+            logging.warn("Total number of traceroute: {} ({} ignored)\nTotal number of tracks: {} ({} expired, {} ignored, {} empty)".format(nb_total_traceroutes, nb_err_traceroutes, tm.nb_tracks, tm.nb_expired_tracks, tm.nb_ignored_tracks, tm.nb_empty_tracks))
 
-        if nb_total_traceroutes % gc_expiration == 0:
-            logging.warn("Running garbage collection")
-            logging.warn("Total number of traceroute: {} ({} ignored, {} expired)".format(nb_total_traceroutes, nb_err_traceroutes, nb_expired_traceroutes))
-            expired_bins = []
-            for date, tracks in track_bins.iteritems():
+tm.collect_results(results, dates, force_expiration=0.5)
 
-                # logging.warn("preProcessing bin {}{}".format(date))
-                # TODO: probe diversity
-
-                if bins_last_insert[date]+gc_expiration < nb_total_traceroutes:
-                    logging.warn("Processing bin {}".format(date))
-                    expired_bins.append(date)
-                    
-                    diffrtt = defaultdict(list)
-                    for track in tracks:
-                        for locPair in combinations(track["rtts"],2):
-                            (loc0, rtts0) = locPair[0]
-                            (loc1, rtts1) = locPair[1]
-                            diffrtt[(loc0,loc1)] += [ x1-x0 for x0,x1 in product(rtts0, rtts1)]
-
-                    # Compute median/wilson scores and shift windows
-                    if len(diffrtt):
-                        for locations, dr in diffrtt.iteritems():
-                            results[str(locations)]["median"].append(np.median(dr))
-                            results[str(locations)]["nb_samples"].append(len(dr))
-
-            # Garbage collection
-            # remember expired dates and delete corresponding bins
-            for date in expired_bins:
-                bins_last_insert[date] = None 
-                del track_bins[date]
-                # print("Removing bin {}, {}".format(locations, date))
-            logging.warn("Running python garbage collection")
-            gc.collect()
-
-for date, values in bins_last_insert.iteritems():
-    dates.append(date*window_size)
-                            
-
-logging.warn("Total number of traceroute: {} ({} ignored, {} expired)".format(nb_total_traceroutes, nb_err_traceroutes, nb_expired_traceroutes))
 pickle.dump(results, open("results.pickle", "wb"))
 pickle.dump(dates, open("dates.pickle", "wb"))
---
+++ src/tracksmanager.py
@@ -0,0 +1,89 @@
+import logging
+import numpy as np
+from collections import defaultdict 
+from itertools import combinations, product
+
+
+class TracksManager():
+
+    def __init__(self, window_size, expiration):
+        self.window_size = window_size
+        self.nb_ignored_tracks = 0
+        self.nb_empty_tracks = 0
+        self.nb_tracks = 0
+        self.track_bins = {}
+        self.bins_last_insert= defaultdict(int)
+        self.expiration = expiration
+        self.nb_expired_bins = 0
+        self.nb_expired_tracks = 0
+
+    def add_track(self, track):
+        """Add a new track to the history."""
+
+        self.nb_tracks += 1
+        if track is None:
+            self.nb_empty_tracks += 1
+            return
+
+        bin_id = int(track["timestamp"]/self.window_size)
+        if self.bins_last_insert[bin_id] is None:
+            self.nb_ignored_tracks += 1
+            return
+
+        # index track based on its timestamp
+        if bin_id not in self.track_bins:
+            self.track_bins[bin_id] = []
+
+        self.track_bins[bin_id].append(track)
+        self.bins_last_insert[bin_id] = self.nb_tracks
+
+
+    def compute_median_diff_rtt(self, tracks, results):
+        diffrtt = defaultdict(list)
+        for track in tracks:
+            for locPair in combinations(track["rtts"],2):
+                (loc0, rtts0) = locPair[0]
+                (loc1, rtts1) = locPair[1]
+                diffrtt[(loc0,loc1)] += [ x1-x0 for x0,x1 in product(rtts0, rtts1)]
+
+        # Compute median/wilson scores and shift windows
+        # TODO wilson
+        if len(diffrtt):
+            for locations, dr in diffrtt.iteritems():
+                dra = np.array(dr, copy=False)
+                results[str(locations)]["median"].append(np.median(dra))
+                results[str(locations)]["nb_samples"].append(len(dra))
+
+    def collect_results(self, results, dates, force_expiration=0):
+        """Find expired track bins, that is bins that are not affected by the 
+        last n track insertions (n=self.expiration). And compute median differential
+        RTTs, number of sample, etc.. for expired bins. """
+
+        if self.nb_tracks % self.expiration == 0 or force_expiration:
+
+            logging.warn("Running results collection")
+            expired_bins = []
+            for date, tracks in self.track_bins.iteritems():
+
+                # TODO: add probe diversity
+
+                if self.bins_last_insert[date]+self.expiration < self.nb_tracks or force_expiration:
+                    if force_expiration and not len(tracks)>force_expiration*(self.nb_expired_tracks/self.nb_expired_bins):
+                        continue
+                    else:
+                        logging.debug("Force expiration for bin {}".format(date))
+
+                    logging.warn("Processing bin {}".format(date))
+                    expired_bins.append(date)
+
+                    self.compute_median_diff_rtt(tracks, results)
+                    dates.append(date*self.window_size+self.window_size/2)
+                    self.nb_expired_tracks += len(tracks)
+                    self.nb_expired_bins += 1
+
+            # remember expired dates and delete corresponding bins
+            for date in expired_bins:
+                self.bins_last_insert[date] = None 
+                del self.track_bins[date]
+
+
