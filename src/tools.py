f73f2b5079e2a6cd74d24ea3952ef22e6064b0eb
romain.fontugne <romain.fontugne@gmail.com>
2018-07-11 12:36:39 +0900

    TODO handle private IPs for IPv6

--- src/tools.py
+++ src/tools.py
@@ -8,6 +8,8 @@ priv_24 = re.compile("^10\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 priv_20 = re.compile("^192\.168\.\d{1,3}.\d{1,3}$")
 priv_16 = re.compile("^172.(1[6-9]|2[0-9]|3[0-1]).[0-9]{1,3}.[0-9]{1,3}$")
 
+#TODO private network for ipv6
+
 def isPrivateIP(ip):
 
     return priv_lo.match(ip) or priv_24.match(ip) or priv_20.match(ip) or priv_16.match(ip)
