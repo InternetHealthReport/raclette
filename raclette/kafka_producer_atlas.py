33f64e70291abb4f293ef9db8c8033f3f6a43342
romain.fontugne <romain.fontugne@gmail.com>
2019-06-18 04:04:32 +0000

    fix measurement IDs for ASC example

--- raclette/kafka_producer_atlas.py
+++ raclette/kafka_producer_atlas.py
@@ -15,32 +15,8 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from concurrent.futures import ThreadPoolExecutor
 
-
 #IMPORT KAFKA PRODUCER
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers=['kafka1:9092', 'kafka2:9092', 'kafka3:9092'],
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
-#end import
-logging.basicConfig()#should be removable soon
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-C","--config_file", help="Get all parameters from the specified config file", type=str, default="conf/raclette.conf")
-args = parser.parse_args()
-
-# Read the config file
-config = configparser.ConfigParser()
-config.read(args.config_file)
-
-atlas_msm_ids =  [int(x) for x in config.get("io", "msm_ids").split(",") if x]
-atlas_probe_ids =  [int(x) for x in config.get("io", "probe_ids").split(",") if x]
-
-atlas_start =  tools.valid_date(config.get("io", "start"))
-atlas_stop =  tools.valid_date(config.get("io", "stop"))
-chunk_size = int(config.get('io', 'chunk_size'))
-
-topic = config.get("io", "kafka_topic")
-
 
 def requests_retry_session(
     retries=3,
@@ -87,34 +63,55 @@ def cousteau_on_steroid(params, retry=3):
 
     session = requests_retry_session()
     for msm in params["msm_id"]:
-        queries.append( session.get(url=url.format(msm), params=req_param,
+        queries.append( [session.get(url=url.format(msm), params=req_param,
                 hooks={ 'response': worker_task, }
-            ) )
+            ), [url.format(msm), req_param]] )
 
-    for query in queries:
+    for tmp in queries:
+        query = tmp[0]
         try:
             resp = query.result()
             yield (resp.ok, resp.data)
         except requests.exceptions.ChunkedEncodingError:
             logging.error("Could not retrieve traceroutes for {}".format(query))
+            logging.error(tmp[1])
 
+if __name__ == '__main__':
+    producer = KafkaProducer(bootstrap_servers=['kafka1:9092', 'kafka2:9092', 'kafka3:9092'],
+                            value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-if (len(sys.argv) >= 3):
-    print("3 Arguments.  Using Start and End Time")
+    #end import
+    logging.basicConfig()#should be removable soon
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-C","--config_file", help="Get all parameters from the specified config file", type=str, default="conf/raclette.conf")
+    args = parser.parse_args()
+
+    # Read the config file
+    config = configparser.ConfigParser()
+    config.read(args.config_file)
+
+    atlas_msm_ids =  [int(x) for x in config.get("io", "msm_ids").split(",") if x]
+    atlas_probe_ids =  [int(x) for x in config.get("io", "probe_ids").split(",") if x]
+
+    atlas_start =  tools.valid_date(config.get("io", "start"))
+    atlas_stop =  tools.valid_date(config.get("io", "stop"))
+    chunk_size = int(config.get('io', 'chunk_size'))
+
+    topic = config.get("io", "kafka_topic")
+
     current_time = atlas_start
     end_time = atlas_stop
     while current_time < end_time:
+        print("downloading: ", current_time)
         params = { "msm_id": atlas_msm_ids, "start": current_time, "stop": current_time  + timedelta(seconds=chunk_size), "probe_ids": atlas_probe_ids }
 
         for is_success, data in cousteau_on_steroid(params):
-            print("downloading")
             if is_success:
                 for traceroute in data:
                     producer.send(topic, value=traceroute, timestamp_ms = traceroute.get('timestamp'))
-                    producer.flush()
             else:
                 print("Error could not load the data")
 
+        producer.flush()
         current_time = current_time + timedelta(seconds = chunk_size)
-else:
-    print("Improper argument use.  Need either none or exactly 2, first start time, then end time")
